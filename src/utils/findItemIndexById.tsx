af6bfe0fcfe4d97d8f7ddad88b9cc9346be472f2
Yetife <tifetreats21@gmail.com>
2022-08-12 17:16:47 +0100

    New files

---
+++ src/utils/findItemIndexById.tsx
@@ -0,0 +1,8 @@
+// @ts-ignore
+interface Item {
+    id: string
+}
+
+export const findItemIndexById = <T extends Item>(items: T[], id: string) => {
+    return items.findIndex((item: T) => item.id === id)
+}
