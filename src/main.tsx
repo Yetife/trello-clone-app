a90c30a6e864da35732a9bfc118979c8fc19dae6
Yetife <tifetreats21@gmail.com>
2022-10-14 18:08:06 +0100

    New files

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { DndProvider } from "react-dnd"
+// @ts-ignore
+import {Backend}from "react-dnd-html5-backend"
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import {AppStateProvider} from "./AppStateContext";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <AppStateProvider>
-    <App />
-  </AppStateProvider>
+    // @ts-ignore
+    <DndProvider backend={ HTML5Backend}>
+        <AppStateProvider>
+            <App />
+        </AppStateProvider>
+    </DndProvider>
 )
